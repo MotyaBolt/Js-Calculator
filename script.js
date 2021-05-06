202b9933c160d6090bbfd3c8b94827cf78a67861
Matvey Boltach <66522023+MotyaBolt@users.noreply.github.com>
2021-05-06 15:31:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,181 +2,134 @@ let currentValue = 0;
 let action = 0;
 let result = 0;
 let nextValue = 0;
-function showDefault () {
-    let display = document.getElementById('display');
-    display.innerHTML = 0;
-    currentValue = 0;
-    result = 0;
-    action = 0;
-    nextValue = 0;
-}
-function showOnDisplay(idOfButton) {
-let display = document.getElementById('display');
-let button = document.getElementById(idOfButton);
-    if(display.innerHTML === "0") {
-        display.innerHTML = button.value;
-        if(idOfButton === 'decimal') {
-            display.innerHTML = "0."
-        }
+
+const showDefault = () => {
+  let display = document.getElementById('display');
+  display.innerHTML = 0;
+  currentValue = 0;
+  result = 0;
+  action = 0;
+  nextValue = 0;
+};
+
+const showOnDisplay = (idOfButton) => {
+  let display = document.getElementById('display');
+  let button = document.getElementById(idOfButton);
+  if (display.innerHTML === '0') {
+    display.innerHTML = button.value;
+    if (idOfButton === 'decimal') {
+      display.innerHTML = '0.';
     }
-    else   {
-        display.innerHTML += button.value;
-        if(idOfButton === 'decimal') {
-            if(!display.innerHTML.includes("."))
-            display.innerHTML += "."
-           }
+  } else {
+    display.innerHTML += button.value;
+    if (idOfButton === 'decimal') {
+      if (!display.innerHTML.includes('.')) display.innerHTML += '.';
     }
-}
-function calculateVal(idOfButton) {
-let display = document.getElementById('display');
-let button = document.getElementById(idOfButton);
-if(idOfButton === 'add') {
+  }
+};
+
+const calculateVal = (idOfButton) => {
+  let display = document.getElementById('display');
+  let button = document.getElementById(idOfButton);
+  if (idOfButton === 'add') {
     nextValue = display.innerHTML;
-    console.log(nextValue);
-    if(action === '-') {
-        result = parseFloat(currentValue) - parseFloat(nextValue);
+    if (action === '-') {
+      result = parseFloat(currentValue) - parseFloat(nextValue);
+    } else if (action === '*') {
+      if (nextValue === '0') {
+        result = currentValue;
+      } else {
+        result = parseFloat(currentValue) * parseFloat(nextValue);
+      }
+    } else if (action === '/') {
+      result = parseFloat(currentValue) / parseFloat(nextValue);
+    } else {
+      result = parseFloat(currentValue) + parseFloat(nextValue);
     }
-    else if(action === '*') {
-        if(nextValue === '0') {
-            result = currentValue;
-            }
-            else{
-            result = parseFloat(currentValue) * parseFloat(nextValue);
-            }
+    if (currentValue === 0) {
+      currentValue = parseFloat(display.innerHTML);
+    } else {
+      currentValue = result;
     }
-    else if(action === '/') {
-        result = parseFloat(currentValue) / parseFloat(nextValue);
-    }
-    else { 
-    result = parseFloat(currentValue) + parseFloat(nextValue);
-    }
-    console.log(result)
-     if(currentValue === 0) {
-        currentValue = parseFloat(display.innerHTML);
-        }
-        else {
-            currentValue  = result
-        }
-        console.log(currentValue)
     display.innerHTML = 0;
     action = '+';
-    console.log(action)
-    
-}
-else if(idOfButton === 'subtract') {
-        nextValue = display.innerHTML
-        console.log(nextValue);
-        if(action === '+') {
-            result = parseFloat(currentValue) + parseFloat(nextValue);
-        }
-        else if(action === '*' ) {
-            if(nextValue === '0') {
-            result = currentValue;
-            }
-            else{
-            result = parseFloat(currentValue) * parseFloat(nextValue);
-            }
-        }
-        else if(action === '/') {
-                result = parseFloat(currentValue) / parseFloat(nextValue);
-        }
-        else {
-        result = parseFloat(currentValue) - parseFloat(nextValue);
-       
-        }
-        console.log(result)
-        if(currentValue === 0) {
-        currentValue = parseFloat(display.innerHTML);
-        }
-        else {
-            currentValue = result;
-        }
-    console.log(currentValue)
+  } else if (idOfButton === 'subtract') {
+    nextValue = display.innerHTML;
+    if (action === '+') {
+      result = parseFloat(currentValue) + parseFloat(nextValue);
+    } else if (action === '*') {
+      if (nextValue === '0') {
+        result = currentValue;
+      } else {
+        result = parseFloat(currentValue) * parseFloat(nextValue);
+      }
+    } else if (action === '/') {
+      result = parseFloat(currentValue) / parseFloat(nextValue);
+    } else {
+      result = parseFloat(currentValue) - parseFloat(nextValue);
+    }
+    if (currentValue === 0) {
+      currentValue = parseFloat(display.innerHTML);
+    } else {
+      currentValue = result;
+    }
     display.innerHTML = 0;
     action = '-';
-    console.log(action)
-}
-else if(idOfButton === 'multiply') {
-        
-        nextValue = display.innerHTML
-        console.log(nextValue)
-        if(action === '+') {
-            result = parseFloat(currentValue) + parseFloat(nextValue);
-        }
-        else if(action === '-') {
-            result = parseFloat(currentValue) - parseFloat(nextValue);
-        }
-        else if(action === '/') {
-            result = parseFloat(currentValue) / parseFloat(nextValue);
-        }
-        
-        else {
-        result = parseFloat(currentValue) * parseFloat(nextValue);
-        }
-        console.log(result)
-        if(currentValue === 0) {
-        currentValue = parseFloat(display.innerHTML);
-        }
-        else {
-            currentValue = result;
-        }
-        console.log(currentValue)
+  } else if (idOfButton === 'multiply') {
+    nextValue = display.innerHTML;
+    if (action === '+') {
+      result = parseFloat(currentValue) + parseFloat(nextValue);
+    } else if (action === '-') {
+      result = parseFloat(currentValue) - parseFloat(nextValue);
+    } else if (action === '/') {
+      result = parseFloat(currentValue) / parseFloat(nextValue);
+    } else {
+      result = parseFloat(currentValue) * parseFloat(nextValue);
+    }
+    if (currentValue === 0) {
+      currentValue = parseFloat(display.innerHTML);
+    } else {
+      currentValue = result;
+    }
     display.innerHTML = 0;
     action = '*';
-    console.log(action)
-}
-else if(idOfButton === 'divide') {
-    nextValue = display.innerHTML
-    console.log(nextValue)
-    if(action === '+') {
-        result = parseFloat(currentValue) + parseFloat(nextValue);
+  } else if (idOfButton === 'divide') {
+    nextValue = display.innerHTML;
+    if (action === '+') {
+      result = parseFloat(currentValue) + parseFloat(nextValue);
+    } else if (action === '-') {
+      result = parseFloat(currentValue) - parseFloat(nextValue);
+    } else if (action === '*') {
+      result = parseFloat(currentValue) * parseFloat(nextValue);
+    } else {
+      result = parseFloat(currentValue) / parseFloat(nextValue);
     }
-    else if(action === '-') {
-        result = parseFloat(currentValue) - parseFloat(nextValue);
+    if (currentValue === 0) {
+      currentValue = parseFloat(display.innerHTML);
+    } else {
+      currentValue = result;
     }
-    else if(action === '*') {
-        result = parseFloat(currentValue) * parseFloat(nextValue);
-    }
-    
-    else {
-    result = parseFloat(currentValue) / parseFloat(nextValue);
-    }
-    console.log(result)
-    if(currentValue === 0) {
-        currentValue = parseFloat(display.innerHTML);
-        }
-        else {
-            currentValue = result;
-        }
-        console.log(currentValue)
     display.innerHTML = 0;
     action = '/';
-    console.log(action)
-}
-else if(idOfButton === 'equals') {
-        if(action === '+') {
-           display.innerHTML = currentValue + parseFloat(display.innerHTML);
-           currentValue = 0;
-        }
-        else if(action === '-') {
-            if(nextValue === '0') {
-                display.innerHTML = currentValue * -parseFloat(display.innerHTML);
-                currentValue = 0;
-            }
-            else {
-            display.innerHTML = currentValue - parseFloat(display.innerHTML)
-            currentValue = 0;
-            }
-        }
-        else if(action === '*') {
-            display.innerHTML = currentValue * parseFloat(display.innerHTML)
-            currentValue = 0;
-        }
-        else if(action === '/') {
-            display.innerHTML = currentValue / parseFloat(display.innerHTML)
-            currentValue = 0;
-        }
-        action = '=';
-        console.log(action)
+  } else if (idOfButton === 'equals') {
+    if (action === '+') {
+      display.innerHTML = currentValue + parseFloat(display.innerHTML);
+      currentValue = 0;
+    } else if (action === '-') {
+      if (nextValue === '0') {
+        display.innerHTML = currentValue * -parseFloat(display.innerHTML);
+        currentValue = 0;
+      } else {
+        display.innerHTML = currentValue - parseFloat(display.innerHTML);
+        currentValue = 0;
+      }
+    } else if (action === '*') {
+      display.innerHTML = currentValue * parseFloat(display.innerHTML);
+      currentValue = 0;
+    } else if (action === '/') {
+      display.innerHTML = currentValue / parseFloat(display.innerHTML);
+      currentValue = 0;
     }
-}
+    action = '=';
+  }
+};
